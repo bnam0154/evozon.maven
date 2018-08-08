64f79403eb110bb5b6df2338cb234ed26631c63b
BoldizsarNorbert <boldizsarnorbertb@gmail.com>
2018-08-08 14:55:19 +0300

    commit from intelIJ IDE

--- src/test/java/com/evozonintern/utils/Constants.java
+++ src/test/java/com/evozonintern/utils/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final String BASE_URL = "http://qa3.dev.evozon.com/";
     public static final String LOCAL_URL = "";
     public static final String CSV_FILE_PATH = "src/test/resources/TestDataFiles/";
-
+//cevasdcssdcscsdcsdsdcscsdcsddc
 }
